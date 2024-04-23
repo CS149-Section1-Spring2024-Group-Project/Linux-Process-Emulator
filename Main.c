46e9b40e1a87f8712180a9de5a49a55f5b73dee8
andynguyen072 <andynguyen072@gmail.com>
2024-04-22 21:20:51 -0700

    added command prompt system

--- Main.c
+++ Main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
 
@@ -23,8 +26,44 @@ int main(int argc, char* argv[]){
     printf("The process manager ensures that no more than one reporter process is running at any moment. \n");
     printf("and then performs scheduling. Note that scheduling may involve performing context switching\n");
     printf("\n");
+    printf("    'Exit'          Abruptly ends the program.\n\n");
+    printf("The program will no longer listen to any user input, ending the program. \n");
 
-    
+    while (true) {
+        char UserInputCommand[] = "";
+        printf("\nEnter Command: ");
+        scanf("%s", UserInputCommand);
+
+        if ((strcmp(UserInputCommand, "Q")) == 0) {
+            /*
+                Q - End of one unit of time
+            */
+            printf("Q command\n");
+        } else if ((strcmp(UserInputCommand, "U")) == 0) {
+            /*
+                U - Unblock the first simulated process in blocked queue
+            */
+            printf("U command\n");
+        } else if ((strcmp(UserInputCommand, "P")) == 0) {
+            /*
+                P - Print the current state of the system
+            */
+            printf("P command\n");
+        } else if ((strcmp(UserInputCommand, "T")) == 0) {
+            /*
+                T - Print the average turnaround time and terminate the system
+            */
+            printf("T command\n");
+        } else if ((strcmp(UserInputCommand, "Exit")) == 0) {
+            /*
+                Exit - Abruptly ends the program
+            */
+            printf("Terminated Program\n");
+            break;
+        } else {
+            printf("Invalid command\n");
+        }
+    }
 
     return 0;
 }
